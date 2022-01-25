bd90a9686af399b399e7cbf791d3985df9b7849c
kimsa1984 <suna5012kim@gmail.com>
2022-01-25 17:41:41 +0900

    dddd

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
- <!-- 메인 슬라이드  -->
+// 메인 슬라이드
         function slide(){
             $('#slide').stop().animate({marginLeft:-1400},function(){
                 $('#slide li:first').appendTo('#slide');
@@ -6,3 +6,18 @@
             });
         }
         setInterval(slide,3500);
+
+// popup iframe
+        $(".click").click(function(){
+            $(".popup").hide();
+            $(".iframe").show();
+            $(".iframe iframe").attr("src","https://player.vimeo.com/video/616246851?title=0&amp;byline=0&amp;portrait=0&amp;h=6bcc005cb8");
+            $(".popup").show();
+            $(".close").show();
+        });
+        $(".close").click(function(){
+            $(".iframe").hide();
+            $(".closebtn").hide();
+           
+        });
+        
